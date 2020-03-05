6c448d74d5899de7951ff9060100b8bc2c6b25ee
Walker <dinil@protonmail.com>
2020-03-06 00:59:08 +0550

    2020/03/06, 00:39:07

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var transition = {
+    '-moz-transition': 'color 0.3s ease-in',
+    '-o-transition': 'color 0.3s ease-in',
+    '-webkit-transition': 'color 0.3s ease-in',
+    'transition': 'color 0.3s ease-in'
+};
+
+var nothing = {
+    '-moz-transition': 'none',
+    '-o-transition': 'none',
+    '-webkit-transition': 'none',
+    'transition': 'none'   
+}
+
 $(document).ready(function () {
     let darkTheme = localStorage.getItem('theme') === 'dark';
     updateTheme();
@@ -10,19 +24,28 @@ $(document).ready(function () {
     function updateTheme() {
         if (darkTheme) {
             $("body").addClass("dark-theme");
-            $("body").removeClass("dynamic-body");
-            $("body").addClass("static-body");
 
         } else {
             $("body").removeClass("dark-theme");
-            $("body").removeClass("static-body");
-            $("body").addClass("dynamic-body");
         }
+    }
+
+    function open_trans() {
+        $("body").removeClass("dynamic-body");
+        $("body").addClass("static-body");
+        $("h1").css(nothing);
+        $("p").css(nothing);
+        
     }
 
     $(".theme-switch").on("click", () => {
         darkTheme = !darkTheme;
         updateTheme();
         localStorage.setItem('theme', darkTheme ? 'dark' : 'light');
+        $("body").removeClass("static-body");
+        $("body").addClass("dynamic-body");
+        $("h1").css(transition);
+        $("p").css(transition);
+        setTimeout(open_trans, 3000)
     });
 });
